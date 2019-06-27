49ed7c5d6c0c03a38334bf6b175a20264251fbc0
fanjiantao <1418154909@qq.com>
2019-06-27 17:40:41 +0800

    测试

--- config.js
+++ config.js
@@ -543,8 +543,81 @@ const config = {
         appKey: '157551',
         appSecret: '29ac3a00-40c3-4ea3-bd77-abd7c1bb6939'
     },
-    qkd6:{
-        
+    qkd6: {
+        //新闻频道
+        newsListAD: [
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4022638&h=120" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4012794&h=120" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4012786&h=120" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4012787&h=120" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4012795&h=120" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4012796&h=120" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4012797&h=120" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4012798&h=120" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4022637&h=120" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4022638&h=120" },
+        ],
+        priclist: [
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u3689775&h=220" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u3689774&h=220" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u3686137&h=220" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u3686127&h=220" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u3689774&h=220" },
+            { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u3689775&h=220" },
+
+        ],
+        //新闻详情
+        details: {
+            //顶部广告
+            topAD: { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058041&h=120" },
+            //内容底部广告
+            bottomAD: { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u3689770&h=220" },
+            //猜你喜欢5图广告
+            IlickAd: { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u3651945&h=400" },
+            //推荐阅读列表广告
+            RecommendList: [
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058055&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058053&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058047&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058046&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058043&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058041&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058037&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058035&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058032&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058030&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058055&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058053&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058047&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058046&h=120" },
+                { type: 'ad', s: 'xcAD', smua: "d=m&s=b&u=u4058043&h=120" },
+            ]
+
+        },
+        //图片详情
+        picDetails: {
+            picGroomIdsList: [
+                { uId: { id: 'u4021427', h: "20:15" }, type: 'xc', coverId: '', callBack: '', countId: 'xcu4021427' },
+                { uId: { id: 'u4021427', h: "20:15" }, type: 'xc', coverId: '', callBack: '', countId: 'xcu4021427' },
+            ],
+            picBottomAdId: [
+                { uId: { id: 'u3794541', h: '20:4' }, type: 'xc', coverId: '', callBack: '', countId: 'xcu3794541' },
+                { uId: { id: 'u3794541', h: '20:4' }, type: 'xc', coverId: '', callBack: '', countId: 'xcu3794541' },
+            ],
+            picAdIdsList: [
+                { uId: { id: 'u4021427', h: "20:15" }, type: 'xc', coverId: '', callBack: '', countId: 'xcu4021427' },
+            ],
+            linkList: 'http://engine.tuicoco.com/index/activity?appKey=KojNnfcjMmWb9NZaTbihqMwBRuc&adslotId=292165',
+            linkDetail: 'http://engine.tuicoco.com/index/activity?appKey=KojNnfcjMmWb9NZaTbihqMwBRuc&adslotId=292167',
+        },
+        //红包链接
+        linkAD: {
+            newslist: 'http://engine.tuicoco.com/index/activity?appKey=KojNnfcjMmWb9NZaTbihqMwBRuc&adslotId=292165',
+            details: 'http://engine.tuicoco.com/index/activity?appKey=KojNnfcjMmWb9NZaTbihqMwBRuc&adslotId=292167',
+        },
+        //密钥相关
+        appKey: '815949',
+        appSecret: '0f171fc7-6a77-41c7-b3dd-f5a18d6e7ed2',
     }
 }
 
